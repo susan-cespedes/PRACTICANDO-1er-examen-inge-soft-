e753f4b2fd4e87bac02d86d01080d2d1bd40db9c
susan-cespedes <susan.cespedes@ucb.edu.bo>
2023-12-12 22:36:11 -0400

    Refactoring

--- src/moneda.js
+++ src/moneda.js
@@ -1,23 +1,15 @@
 
 function cambioMonedas(monto){
-    let lista = [5,2];
-    let otralista = [5,2,2];
-    let otralistamas = [5,5,2,1];
+    let lista = [5,2,1];
+    let cambio = [];
+    lista.forEach((denominacion)=>{
+        let cantidad=Math.floor(monto/denominacion);
+        monto-=cantidad*denominacion;
+        for (let i=0; i<cantidad; i++){
+            cambio.push(denominacion);
+        }
+    })
     
-    if (monto == 13){
-        return otralistamas;
-    }
-    if (monto == 9){
-        return otralista;
-    }
-    if (monto == 7){
-        return lista;
-    }
-    if (monto > 0){
-        return [monto];
-    }
-    else{
-        return [];
-    }
+        return cambio;
 }
 export default cambioMonedas;
