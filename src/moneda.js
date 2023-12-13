2962f4433104ba4ebbc31bd5db34140d4cc13361
susan-cespedes <susan.cespedes@ucb.edu.bo>
2023-12-12 21:54:01 -0400

    devolver un vector con 1

--- src/moneda.js
+++ src/moneda.js
@@ -1,5 +1,10 @@
 
-function cambioMonedas(){
-    return [];
+function cambioMonedas(monto){
+    if (monto > 0){
+        return [1];
+    }
+    else{
+        return [];
+    }
 }
 export default cambioMonedas;
--- src/moneda.test.js
+++ src/moneda.test.js
@@ -4,4 +4,9 @@ describe ("cambio de monedas", () => {
     it ("devolver un vector vacio si el monto ingresado es 0", () => {
         expect (cambioMonedas (0)).toEqual([]);
     });
+
+    it ("devolver un vector con 1", ()=> {
+        expect (cambioMonedas (1)).toEqual([1]);
+    });
+
 });
